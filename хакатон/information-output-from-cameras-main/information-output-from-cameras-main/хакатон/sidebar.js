e9abf48e2fea0e943468b1c3fb6198d595b55756
HappyRoma <85258511+HappyRoma@users.noreply.github.com>
2021-12-05 14:24:21 +0500

    Delete хакатон/information-output-from-cameras-main/information-output-from-cameras-main directory

--- хакатон/information-output-from-cameras-main/information-output-from-cameras-main/хакатон/sidebar.js
+++
@@ -1,9 +0,0 @@
-sidebar = document.querySelector(".sidebar");
-
-sidebar.onmouseover = function () {
-    sidebar.style.right = "-20px";
-}
-
-sidebar.onmouseout = function () {
-    sidebar.style.right = "-130px";
-}
