0b7ba9eedece93a86d7d14452c7376357ec4d14c
Helloeverybody <seva.bocharov@mail.ru>
2021-12-05 18:00:06 +0500

    апи соединено

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,8 @@
+import datetime
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from matrix_creator import get_correspondense_matrix
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -9,7 +12,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 @app.route('/')
 def index():
-    print(request.args.to_dict())
+    get_correspondense_matrix(datetime.datetime.fromtimestamp(int(request.args.to_dict()['date'])))
     return jsonify(request.args.to_dict())
 
 
